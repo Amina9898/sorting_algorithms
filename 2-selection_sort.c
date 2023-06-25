47bfa675c52df2fcf9cd76fdb4b27692635e0559
Frida <amina.amer.9898@gmail.com>
2023-06-25 12:55:35 -0700

    Task 2 output error fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,9 +23,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_index])
 				min_index  = j;
 		}
-		tmp = array[i];
-		array[i] = array[min_index];
-		array[min_index] = tmp;
-		print_array(array, size);
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
 	}
 }
